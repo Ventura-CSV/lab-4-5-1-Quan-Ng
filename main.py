592e80c72878c737ac1bc86a4e06dd72ee5a8bff
Quan-Ng <QNguyen7#07@gmail.com>
2024-06-27 06:50:02 -0700

    4.5.1:DONE

--- main.py
+++ main.py
@@ -7,9 +7,25 @@ def main():
     Code Your Program here
     ########################################
     """
+    import random  
+    
+    list = []
+   
+    numbers_random = 5
+    
+    count = 0
+    
+    count += 1
+    
+    while  count < numbers_random:
+        numbers = random.randint(1,100)
+        
+        list.append(numbers)
+        print(f'The random values are {numbers}')
 
-    print(f'The random values are {numbers}')
-    print(f'The total is {total}')
+total = sum(list)
+    
+print(f'The total is {total}')
 
     ########################################
     # Do not delete the return statement
